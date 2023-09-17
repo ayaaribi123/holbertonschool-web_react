0f9c7549516f073f342889e00f3d200b44ee1e6f
ayaaribi123 <ayaaribi11@gmail.com>
2023-09-17 20:35:12 +0100

    react

--- task_2/dashboard/src/utils.js
+++ task_2/dashboard/src/utils.js
@@ -12,3 +12,9 @@ export function getFooterCopy(isIndex) {
 					return ("Holberton School main dashboard");
 	}
 }
+
+export function getLatestNotification(){
+	return(
+					'<strong>Urgent requirement</strong> - complete by EOD'
+	);
+}
